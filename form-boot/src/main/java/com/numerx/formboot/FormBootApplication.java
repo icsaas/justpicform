b8badaf9dbe6eb312196bbc0f989274686b1a30b
numerx <i@justpic.org>
2019-08-11 15:07:25 +0800

    create sketch :checked_flag:

---
+++ form-boot/src/main/java/com/numerx/formboot/FormBootApplication.java
@@ -0,0 +1,13 @@
+package com.numerx.formboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBootApplication.class, args);
+    }
+
+}
